9b2a4a2ebd28e8619643e5f2be48dfae234de6ca
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-02-28 20:04:29 -0600

    add main router

---
+++ index.route.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const slideRoutes = require("./slide/slide.routes");
+
+const router = express.Router(); // eslint-disable-line new-cap
+
+// TODO: use glob to match *.route files
+
+/** GET /health-check - Check service health */
+router.get("/health-check", (req, res) => res.send("OK"));
+
+// mount user routes at /slides
+router.use("/slides", slideRoutes);
+
+module.exports = router;
