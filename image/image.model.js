331217f1656e90980facbf858e831825d57b3f23
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-03-01 18:21:16 -0600

    add routes, controller, model for images

---
+++ image/image.model.js
@@ -0,0 +1,56 @@
+const httpStatus = require('http-status');
+const mongoose = require("mongoose");
+const APIError = require('../helpers/APIError');
+const Schema = mongoose.Schema;
+
+/**
+ * Define the Image schema
+ */
+const ImageSchema = new Schema({
+  name: String,
+  mimetype: String,
+  md5: String,
+  path: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+/**
+ * Statics
+ */
+ImageSchema.statics = {
+  /**
+   * Get image
+   * @param {ObjectId} id - The objectId of image.
+   * @returns {Promise<Image, APIError>}
+   */
+  get(id) {
+    return this.findById(id)
+      .exec()
+      .then(image => {
+        if (image) {
+          return image;
+        }
+        const err = new APIError("No such image exists!", httpStatus.NOT_FOUND);
+        return Promise.reject(err);
+      });
+  },
+
+  /**
+   * List images in descending order of 'createdAt' timestamp.
+   * @param {number} skip - Number of images to be skipped.
+   * @param {number} limit - Limit number of images to be returned.
+   * @returns {Promise<Image[]>}
+   */
+  list({ skip = 0, limit = 50 } = {}) {
+    return this.find()
+      .sort({ createdAt: -1 })
+      .skip(+skip)
+      .limit(+limit)
+      .exec();
+  }
+};
+
+module.exports = mongoose.model("Image", ImageSchema);
--- index.route.js
+++ index.route.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const slideRoutes = require("./slide/slide.routes");
+const imageRoutes = require("./image/image.routes");
 
 const router = express.Router(); // eslint-disable-line new-cap
-
-// TODO: use glob to match *.route files
 
 /** GET /health-check - Check service health */
 router.get("/health-check", (req, res) => res.send("OK"));
 
 // mount slide routes at /slides
 router.use("/slides", slideRoutes);
+
+// mount slide routes at /images
+router.use("/images", imageRoutes);
 
 module.exports = router;
