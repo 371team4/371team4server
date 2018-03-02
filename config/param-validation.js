d963133aa0d19285e544efaa854129550858d1c8
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-03-01 19:48:17 -0600

    refactor param-validation to make it more concise

--- config/param-validation.js
+++ config/param-validation.js
@@ -1,199 +1,132 @@
 const Joi = require('joi')
 
+// validation schema for title object and its contents
+const titleValidationSchema = {
+  content: Joi.string()
+    .required()
+    .label('title.content'),
+  fontColor: Joi.string()
+    .required()
+    .label('title.fontColor'),
+  fontSize: Joi.string()
+    .required()
+    .label('title.fontSize'),
+  fontWeight: Joi.string()
+    .required()
+    .label('title.fontWeight'),
+  fontStyle: Joi.string()
+    .required()
+    .label('title.fontStyle')
+}
+
+// validation schema for description object and its contents
+const descriptionValidationSchema = {
+  content: Joi.string()
+    .required()
+    .label('description.content'),
+  fontColor: Joi.string()
+    .required()
+    .label('description.fontColor'),
+  fontSize: Joi.string()
+    .required()
+    .label('description.fontSize'),
+  fontWeight: Joi.string()
+    .required()
+    .label('description.fontWeight'),
+  fontStyle: Joi.string()
+    .required()
+    .label('description.fontStyle')
+}
+
+// validation schema for date object and its contents
+const dateValidationSchema = {
+  content: Joi.string()
+    .required()
+    .label('date.content'),
+  fontColor: Joi.string()
+    .required()
+    .label('date.fontColor'),
+  fontSize: Joi.string()
+    .required()
+    .label('date.fontSize'),
+  fontWeight: Joi.string()
+    .required()
+    .label('date.fontWeight'),
+  fontStyle: Joi.string()
+    .required()
+    .label('date.fontStyle')
+}
+
+// validation schema for time object and its contents
+const timeValidationSchema = {
+  content: Joi.string()
+    .required()
+    .label('time.content'),
+  fontColor: Joi.string()
+    .required()
+    .label('time.fontColor'),
+  fontSize: Joi.string()
+    .required()
+    .label('time.fontSize'),
+  fontWeight: Joi.string()
+    .required()
+    .label('time.fontWeight'),
+  fontStyle: Joi.string()
+    .required()
+    .label('time.fontStyle')
+}
+
+// validation schema for meta object and its contents
+const metaValidationSchema = {
+  template: Joi.string()
+    .required()
+    .label('meta.template'),
+  timeout: Joi.string()
+    .required()
+    .label('meta.timeout'),
+  repeatable: Joi.boolean()
+    .required()
+    .label('meta.repeatable'),
+  startDate: Joi.string()
+    .required()
+    .label('meta.startDate'),
+  endDate: Joi.string()
+    .required()
+    .label('meta.endDate')
+}
+
+// validation schema for images array and its contents
+const imagesValidationSchema = Joi.array()
+  .items(
+    Joi.string()
+      .hex()
+      .required()
+      .label('image ids')
+  )
+  .required()
+
 module.exports = {
   // POST /api/slides
   createSlide: {
     body: {
-      title: {
-        content: Joi.string()
-          .required()
-          .label('title.content'),
-        fontColor: Joi.string()
-          .required()
-          .label('title.fontColor'),
-        fontSize: Joi.string()
-          .required()
-          .label('title.fontSize'),
-        fontWeight: Joi.string()
-          .required()
-          .label('title.fontWeight'),
-        fontStyle: Joi.string()
-          .required()
-          .label('title.fontStyle')
-      },
-      description: {
-        content: Joi.string()
-          .required()
-          .label('description.content'),
-        fontColor: Joi.string()
-          .required()
-          .label('description.fontColor'),
-        fontSize: Joi.string()
-          .required()
-          .label('description.fontSize'),
-        fontWeight: Joi.string()
-          .required()
-          .label('description.fontWeight'),
-        fontStyle: Joi.string()
-          .required()
-          .label('description.fontStyle')
-      },
-      date: {
-        content: Joi.string()
-          .required()
-          .label('date.content'),
-        fontColor: Joi.string()
-          .required()
-          .label('date.fontColor'),
-        fontSize: Joi.string()
-          .required()
-          .label('date.fontSize'),
-        fontWeight: Joi.string()
-          .required()
-          .label('date.fontWeight'),
-        fontStyle: Joi.string()
-          .required()
-          .label('date.fontStyle')
-      },
-      time: {
-        content: Joi.string()
-          .required()
-          .label('time.content'),
-        fontColor: Joi.string()
-          .required()
-          .label('time.fontColor'),
-        fontSize: Joi.string()
-          .required()
-          .label('time.fontSize'),
-        fontWeight: Joi.string()
-          .required()
-          .label('time.fontWeight'),
-        fontStyle: Joi.string()
-          .required()
-          .label('time.fontStyle')
-      },
-      meta: {
-        template: Joi.string()
-          .required()
-          .label('meta.template'),
-        timeout: Joi.string()
-          .required()
-          .label('meta.timeout'),
-        repeatable: Joi.boolean()
-          .required()
-          .label('meta.repeatable'),
-        startDate: Joi.string()
-          .required()
-          .label('meta.startDate'),
-        endDate: Joi.string()
-          .required()
-          .label('meta.endDate')
-      },
-      images: Joi.array()
-        .items(
-          Joi.string()
-            .required()
-            .label('image ids')
-        )
-        .required()
+      title: titleValidationSchema,
+      description: descriptionValidationSchema,
+      date: dateValidationSchema,
+      time: timeValidationSchema,
+      meta: metaValidationSchema,
+      images: imagesValidationSchema
     }
   },
 
   // UPDATE /api/slides/:slideId
   updateSlide: {
     body: {
-      title: {
-        content: Joi.string()
-          .required()
-          .label('title.content'),
-        fontColor: Joi.string()
-          .required()
-          .label('title.fontColor'),
-        fontSize: Joi.string()
-          .required()
-          .label('title.fontSize'),
-        fontWeight: Joi.string()
-          .required()
-          .label('title.fontWeight'),
-        fontStyle: Joi.string()
-          .required()
-          .label('title.fontStyle')
-      },
-      description: {
-        content: Joi.string()
-          .required()
-          .label('description.content'),
-        fontColor: Joi.string()
-          .required()
-          .label('description.fontColor'),
-        fontSize: Joi.string()
-          .required()
-          .label('description.fontSize'),
-        fontWeight: Joi.string()
-          .required()
-          .label('description.fontWeight'),
-        fontStyle: Joi.string()
-          .required()
-          .label('description.fontStyle')
-      },
-      date: {
-        content: Joi.string()
-          .required()
-          .label('date.content'),
-        fontColor: Joi.string()
-          .required()
-          .label('date.fontColor'),
-        fontSize: Joi.string()
-          .required()
-          .label('date.fontSize'),
-        fontWeight: Joi.string()
-          .required()
-          .label('date.fontWeight'),
-        fontStyle: Joi.string()
-          .required()
-          .label('date.fontStyle')
-      },
-      time: {
-        content: Joi.string()
-          .required()
-          .label('time.content'),
-        fontColor: Joi.string()
-          .required()
-          .label('time.fontColor'),
-        fontSize: Joi.string()
-          .required()
-          .label('time.fontSize'),
-        fontWeight: Joi.string()
-          .required()
-          .label('time.fontWeight'),
-        fontStyle: Joi.string()
-          .required()
-          .label('time.fontStyle')
-      },
-      meta: {
-        template: Joi.string()
-          .required()
-          .label('meta.template'),
-        timeout: Joi.string()
-          .required()
-          .label('meta.timeout'),
-        repeatable: Joi.boolean()
-          .required()
-          .label('meta.repeatable'),
-        startDate: Joi.string()
-          .required()
-          .label('meta.startDate'),
-        endDate: Joi.string()
-          .required()
-          .label('meta.endDate')
-      },
-      images: Joi.array()
-        .items(
-          Joi.string()
-            .required()
-            .label('image ids')
-        )
-        .required()
+      title: titleValidationSchema,
+      description: descriptionValidationSchema,
+      date: dateValidationSchema,
+      time: timeValidationSchema,
+      meta: metaValidationSchema,
+      images: imagesValidationSchema
     },
     params: {
       slideId: Joi.string()
