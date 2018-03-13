0927289f09fe774cc7be8cf7ecb2454c30a4fe3a
KingMackle <mahmoud.mahmoud@usask.ca>
2018-03-13 02:42:28 -0600

    fix images test case

--- test/image.test.js
+++ test/image.test.js
@@ -1,44 +1,21 @@
 const mongoose = require('mongoose')
 const request = require('supertest')
 const httpStatus = require('http-status')
-const chai = require('chai') // eslint-disable-line import/newline-after-import
-const expect = chai.expect
+const chai = require('chai')
 const path = require('path')
 const { app, server } = require('../index')
 const config = require('../src/config/config')
 const seed = require('../src/config/seed')
 
-
+const expect = chai.expect
 chai.config.includeStack = true
 chai.config.truncateThreshold = 0
 
-const initialImages = [
-  {
-    _id: '5a98ad9a16608d51864ef439',
-    name: 'glass.jpg',
-    mimetype: 'image/png',
-    md5: '1aff56d3ac29f45b6283917d3b5d3adb',
-    path: '/images/glass.jpg'
-  },
-  {
-    _id: '5a98ada216608d51864ef43c',
-    name: 'google.jpg',
-    mimetype: 'image/jpeg',
-    md5: '7864df87770667b3d7e291a5f6642a14',
-    path: '/images/google.jpg'
-  },
-  {
-    _id: '5a98ad9a16608d51864ef43b',
-    name: 'androidparty.jpg',
-    mimetype: 'image/jpeg',
-    md5: '5cc6106614b3d9a3feb886e0fee9c25e',
-    path: '/images/androidparty.jpg'
-  }
-]
+const initialImages = JSON.parse(JSON.stringify(seed.initialImages))
 
-const imageIds = ['5a98ad9a16608d51864ef439','5a98ada216608d51864ef43c','5a98ad9a16608d51864ef43b']
+const imageIds = ['5a98ad9a16608d51864ef439', '5a98ada216608d51864ef43c', '5a98ad9a16608d51864ef43b']
 
-var newId;
+let newId, token
 
 describe('## Image APIs', function () {
   this.timeout(15000)
@@ -48,76 +25,83 @@ describe('## Image APIs', function () {
       .connect(mongoUri, { keepAlive: 1 })
       .then(async () => {
         await seed.initImagesCollection()
-        done()
+        try {
+          const res = await request(app).post('/api/login').send({ username: 'test', password: 'admin001' })
+          if (res) {
+            token = res.body.token
+            done()
+          }
+        } catch(e) {
+          console.log(e)
+          done()
+        }
       })
       .catch(e => console.error(e))
   })
 
   after(done => {
-    // may need to enable these once mocha watch is working
-    //mongoose.models = {}
-    //mongoose.modelSchemas = {}
-    //mongoose.connection.close()
     server.close()
     done()
   })
 
   describe('# GET /api/images', () => {
-    it('should return all images', (done) => {
+    it('should return all images', done => {
       request(app)
         .get('/api/images')
         .expect(httpStatus.OK)
-        .then((res) => {
-          for(var i = 0;i < 3;i++){
-            expect(imageIds).to.include(res.body[i]._id);
+        .then(res => {
+          for (var i = 0; i < 3; i++) {
+            expect(imageIds).to.include(res.body[i]._id)
           }
           done()
         })
         .catch(done)
-    });
+    })
   })
 
   describe('# POST /api/images', () => {
-    it('should create a new image', (done) => {
+    it('should create a new image', done => {
       request(app)
-        .put('/api/images')
-        .attach('image',path.join(__dirname, './Image/001.jpg'))
+      .put('/api/images')
+      .set('x-access-token', token)
+        .attach('image', path.join(__dirname, './Image/001.jpg'))
         .expect(httpStatus.OK)
-        .then((res) => {
+        .then(res => {
           expect(res.body.name).to.equal('001.jpg')
           newId = res.body._id
           done()
         })
         .catch(done)
-    });
+    })
   })
 
   describe('# GET /api/images/:imageId', () => {
-    it('should return one images', (done) => {
+    it('should return one images', done => {
       request(app)
         .get(`/api/images/${initialImages[0]._id}`)
+        .set('x-access-token', token)
         .expect(httpStatus.OK)
-        .then((res) => {
+        .then(res => {
           expect(res.body.md5).to.deep.equal(initialImages[0].md5)
           expect(res.body.mimetype).to.deep.equal(initialImages[0].mimetype)
           expect(res.body.name).to.deep.equal(initialImages[0].name)
           done()
         })
         .catch(done)
-    });
+    })
   })
 
-  describe('# DELETE /api/images/:userId', () => {
-    it('should delete image', (done) => {
+  describe('# DELETE /api/images/:imageId', () => {
+    it('should delete image', done => {
       request(app)
         .delete(`/api/images/${newId}`)
+        .set('x-access-token', token)
         .expect(httpStatus.OK)
-        .then((res) => {
+        .then(res => {
           expect(res.body.name).to.equal('001.jpg')
-          done();
+          done()
         })
         .catch(done)
     })
   })
 })
-
