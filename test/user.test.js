d14413d46710c927d6ac4154e9e7cffe510a6e09
KingMackle <mahmoud.mahmoud@usask.ca>
2018-03-13 02:42:57 -0600

    fix users test case

--- test/user.test.js
+++ test/user.test.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
-const request = require('supertest');
-const httpStatus = require('http-status');
-const chai = require('chai'); // eslint-disable-line import/newline-after-import
-const expect = chai.expect;
+const mongoose = require('mongoose')
+const request = require('supertest')
+const httpStatus = require('http-status')
+const chai = require('chai')
 const { app, server } = require('../index')
-const config = require('../src/config/config');
-const seed = require('../src/config/seed');
+const config = require('../src/config/config')
+const seed = require('../src/config/seed')
 
-chai.config.includeStack = true;
+const expect = chai.expect
+chai.config.includeStack = true
+
+let token
 
 describe('## User APIs', function () {
   this.timeout(15000)
@@ -17,87 +19,100 @@ describe('## User APIs', function () {
       .connect(mongoUri, { keepAlive: 1 })
       .then(async () => {
         await seed.initUsersCollection()
-        done()
+        try {
+          const res = await request(app)
+            .post('/api/login')
+            .send({ username: 'test', password: 'admin001' })
+          if (res) {
+            token = res.body.token
+            done()
+          }
+        } catch (e) {
+          console.log(e)
+          done()
+        }
       })
       .catch(e => console.error(e))
   })
 
   after(done => {
-    // may need to enable these once mocha watch is working
-    //mongoose.models = {}
-    //mongoose.modelSchemas = {}
-    //mongoose.connection.close()
     server.close()
     done()
   })
 
-  let user = {
+  const user = {
     username: 'KK123',
-    password: '1234567890'
+    password: '1234567890',
+    email: 'email@email.com'
   }
 
-  let noNameUser = {
-    password : 'helloworld'
+  const noNameUser = {
+    password: 'helloworld'
   }
 
-  let noPasswordUser = {
-    username : 'admin'
+  const noPasswordUser = {
+    username: 'admin'
   }
 
   describe('# POST /api/users', () => {
-    it('should create a new user', (done) => {
+    it('should create a new user', done => {
       request(app)
         .post('/api/users')
+        .set('x-access-token', token)
         .send(user)
         .expect(httpStatus.OK)
-        .then((res) => {
+        .then(res => {
           expect(res.body.username).to.equal(user.username)
-          expect(res.body.password).to.equal(user.password)
-          user = res.body
+          //console.log(jwt.verify(JSON.stringify(res.body.password), config.jwtSecret))
+          //expect(res.body.password).to.equal(user.password)
+          user._id = res.body._id
           done()
         })
         .catch(done)
-    });
+    })
   })
 
   describe('# POST /api/users', () => {
-    it('should create a new user', (done) => {
+    it('should create a new user', done => {
       request(app)
         .post('/api/users')
+        .set('x-access-token', token)
         .send(noNameUser)
         .expect(httpStatus.BAD_REQUEST)
-        .then((res) => {
-          expect(res.body.message).to.equal('\'username\' is required')
+        .then(res => {
+          expect(res.body.message).to.equal("'username' is required and 'email' is required")
           done()
         })
         .catch(done)
-    });
+    })
   })
 
   describe('# POST /api/users', () => {
-    it('should create a new user', (done) => {
+    it('should create a new user', done => {
       request(app)
         .post('/api/users')
+        .set('x-access-token', token)
         .send(noPasswordUser)
         .expect(httpStatus.BAD_REQUEST)
-        .then((res) => {
-          expect(res.body.message).to.equal('\'password\' is required')
+        .then(res => {
+          expect(res.body.message).to.equal("'password' is required and 'email' is required")
           done()
         })
         .catch(done)
-    });
+    })
   })
 
   describe('# PUT /api/users/:userId', () => {
-    it('should update user details', (done) => {
-      user.password = 'KK'
+    it('should update user details', done => {
+      user.password = 'KK123453'
       request(app)
         .put(`/api/users/${user._id}`)
+        .set('x-access-token', token)
         .send(user)
         .expect(httpStatus.OK)
-        .then((res) => {
+        .then(res => {
           expect(res.body.username).to.equal(user.username)
-          expect(res.body.password).to.equal('KK')
+          //expect(res.body.password).to.equal('KK')
           done()
         })
         .catch(done)
@@ -105,18 +120,17 @@ describe('## User APIs', function () {
   })
 
   describe('# DELETE /api/users/:userId', () => {
-    it('should delete user', (done) => {
+    it('should delete user', done => {
       request(app)
         .delete(`/api/users/${user._id}`)
+        .set('x-access-token', token)
         .expect(httpStatus.OK)
-        .then((res) => {
+        .then(res => {
           expect(res.body.username).to.equal(user.username)
-          expect(res.body.password).to.equal('KK')
-          done();
+          //expect(res.body.password).to.equal('KK')
+          done()
         })
         .catch(done)
     })
   })
-
 })
-
