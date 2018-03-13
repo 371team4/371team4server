78a237813965d95353e0562b42929a05d7010b7b
KingMackle <mahmoud.mahmoud@usask.ca>
2018-03-13 02:42:46 -0600

    fix slides test case

--- test/slide.test.js
+++ test/slide.test.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const request = require('supertest')
 const httpStatus = require('http-status')
-const chai = require('chai') // eslint-disable-line import/newline-after-import
-const expect = chai.expect
+const chai = require('chai')
 const { app, server } = require('../index')
 const config = require('../src/config/config')
 const seed = require('../src/config/seed')
 
+const expect = chai.expect
 chai.config.includeStack = true
 chai.config.truncateThreshold = 0
 
@@ -14,29 +14,29 @@ let newId
 
 const slideTitles = [
   {
-    "content": "Third",
-    "fontColor": "Blue",
-    "fontSize": "X-Large",
-    "fontWeight": "Normal",
-    "fontStyle": "Italic"
+    content: 'Third',
+    fontColor: 'Blue',
+    fontSize: 'X-Large',
+    fontWeight: 'Normal',
+    fontStyle: 'Italic'
   },
   {
-    "content": "first",
-    "fontColor": "Blue",
-    "fontSize": "Small",
-    "fontWeight": "Normal",
-    "fontStyle": "Italic"
+    content: 'first',
+    fontColor: 'Blue',
+    fontSize: 'Small',
+    fontWeight: 'Normal',
+    fontStyle: 'Italic'
   },
   {
-    "content": "Second",
-    "fontColor": "Blue",
-    "fontSize": "Smaller",
-    "fontWeight": "Normal",
-    "fontStyle": "Italic"
+    content: 'Second',
+    fontColor: 'Blue',
+    fontSize: 'Smaller',
+    fontWeight: 'Normal',
+    fontStyle: 'Italic'
   }
 ]
 
-let samplePostSlide ={
+let samplePostSlide = {
   title: {
     content: 'some',
     fontColor: 'Blue',
@@ -75,7 +75,7 @@ let samplePostSlide ={
   images: ['5a98ada216608d51864ef43c', '5a98ad9a16608d51864ef439', '5a98ad9a16608d51864ef43b']
 }
 
-let sampleUpdateSlide ={
+let sampleUpdateSlide = {
   title: {
     content: 'someNew',
     fontColor: 'Blue',
@@ -125,102 +125,94 @@ describe('## Auth APIs', function () {
         await seed.initSlidesCollection()
         await seed.initUsersCollection()
         // how should I call await and async here ?
-        request(app)
-                .post('/api/login')
-                .send({
-                  username: 'test',
-                  password: 'test'
-                })
-                .then((res) => {
-                  samplePostSlide.token = res.body.token
-                  sampleUpdateSlide.token = res.body.token
-                    done()
-                  })
-                .catch(e => console.error(e))
+        try {
+          const res = await request(app).post('/api/login').send({ username: 'test', password: 'admin001' })
+          if (res) {
+            samplePostSlide.token = res.body.token
+            sampleUpdateSlide.token = res.body.token
+            done()
+          }
+        } catch(e) {
+          console.log(e)
+          done()
+        }
       })
       .catch(e => console.error(e))
   })
 
   after(done => {
-    // may need to enable these once mocha watch is working
-    //mongoose.models = {}
-    //mongoose.modelSchemas = {}
-    //mongoose.connection.close()
     server.close()
     done()
   })
 
   describe('# GET /api/slides', () => {
-    it('should return all slides', (done) => {
+    it('should return all slides', done => {
       request(app)
         .get('/api/slides')
         .expect(httpStatus.OK)
-        .then((res) => {
-          for(var i = 0;i < 3;i++){
-           expect(slideTitles).to.deep.include(res.body[i].title);
+        .then(res => {
+          for (var i = 0; i < 3; i++) {
+            expect(slideTitles).to.deep.include(res.body[i].title)
           }
           done()
         })
         .catch(done)
-    });
+    })
   })
 
   describe('# POST /api/slides', () => {
-    it('should create a new slide', (done) => {
+    it('should create a new slide', done => {
       request(app)
         .post('/api/slides')
-        .send( samplePostSlide)
+        .send(samplePostSlide)
         .expect(httpStatus.OK)
-        .then((res) => {
+        .then(res => {
           expect(res.body.title).to.deep.equal(samplePostSlide.title)
           newId = res.body._id
           done()
         })
         .catch(done)
-    });
+    })
   })
 
   describe('# GET /api/slides', () => {
-    it('should return all slides', (done) => {
+    it('should return all slides', done => {
       request(app)
         .get(`/api/slides/${newId}`)
         .expect(httpStatus.OK)
-        .then((res) => {
+        .then(res => {
           expect(res.body.title).to.deep.equal(samplePostSlide.title)
           done()
         })
         .catch(done)
-    });
+    })
   })
 
   describe('# PUT /api/slides/slideId', () => {
-    it('should update a current slide', (done) => {
+    it('should update a current slide', done => {
       request(app)
         .put(`/api/slides/${newId}`)
         .send(sampleUpdateSlide)
         .expect(httpStatus.OK)
-        .then((res) => {
+        .then(res => {
           expect(res.body.title).to.deep.equal(sampleUpdateSlide.title)
           done()
         })
         .catch(done)
-    });
+    })
   })
 
   describe('# DELETE /api/slides/slideId', () => {
-    it('should DELETE a current slide', (done) => {
+    it('should DELETE a current slide', done => {
       request(app)
         .delete(`/api/slides/${newId}`)
         .send(sampleUpdateSlide)
         .expect(httpStatus.OK)
-        .then((res) => {
+        .then(res => {
           expect(res.body.title).to.deep.equal(sampleUpdateSlide.title)
           done()
         })
         .catch(done)
-    });
+    })
   })
-
-
 })
-
